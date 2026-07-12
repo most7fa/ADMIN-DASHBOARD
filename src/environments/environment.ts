9d8044998da663b1e9be1b0e8f2d15697aded28b
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-12 17:13:22 +0300

    edit environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAAqbXLE4srt-fnbbzOCxIUYAXvXXX4U1Q",
+    authDomain: "icon-wear.firebaseapp.com",
+    projectId: "icon-wear",
+    storageBucket: "icon-wear.firebasestorage.app",
+    messagingSenderId: "81150216465",
+    appId: "1:81150216465:web:f65008e0e7fd25014464b0"
+  }
+};
