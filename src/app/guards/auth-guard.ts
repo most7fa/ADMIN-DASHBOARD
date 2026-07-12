4d8c6234a1466e5340d65c316ad8afd16379e4f4
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-12 17:14:36 +0300

    edit auth-guard

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,30 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
+
+export const authGuard: CanActivateFn = async () => {
+
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  return new Promise<boolean>((resolve) => {
+
+    auth.onAuthStateChanged((user) => {
+
+      if (user) {
+
+        resolve(true);
+
+      } else {
+
+        router.navigate(['/']);
+
+        resolve(false);
+
+      }
+
+    });
+
+  });
+
+};
